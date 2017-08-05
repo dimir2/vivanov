22d9c02e87509e62606b21c8a139dab8f5f7324e
Vladimir Ivanov <dimir.ivanov@gmail.com>
2017-08-05 20:28:04 +0300

    1.3. JavaDoc. Code convention - task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,24 @@
 package ru.job4j;
 
+/** 
+ * Class Calculate 
+ * @author Vladimir Ivanov
+ * @version 0.1
+ * @since 05.08.2017
+*/
 public class Calculate {
+	/**
+	 * Echos given values tree times 
+	 * @param value Value to be echoed
+	 * @return Echo string
+	*/
 	public String echo(String value) {
 		return String.format("%s %s %s", value, value, value);
 	}
+
+	/**
+	 * Entry point of the Calculate class
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		Calculate calc = new Calculate();
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vladimir Ivanov (mailto:dimir.ivanov@gmail.com)
+* @version $Id$
+* @since 05.08.2017
+*/
+package ru.job4j;
