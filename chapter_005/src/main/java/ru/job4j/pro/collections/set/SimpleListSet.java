4a99e6da746e8106b40e34ae58425f136dfaa363
Vladimir Ivanov <dimir.ivanov@gmail.com>
2017-08-30 21:25:55 +0300

    2. Set реализованный на связном списке. - task 997

---
+++ chapter_005/src/main/java/ru/job4j/pro/collections/set/SimpleListSet.java
@@ -0,0 +1,154 @@
+package ru.job4j.pro.collections.set;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class SimpleListSet.
+ *
+ * @param <T> Any object.
+ * @author Vladimir Ivanov
+ * @version 0.1
+ * @since 30.08.2017
+ */
+public class SimpleListSet<T> implements SimpleSet<T> {
+    /**
+     * Head of the list.
+     */
+    private Node<T> head;
+    /**
+     * Tail of the list.
+     */
+    private Node<T> tail;
+    /**
+     * Size of container.
+     */
+    private int size;
+
+    /**
+     * Constructs list.
+     */
+    public SimpleListSet() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    /**
+     * Size set.
+     *
+     * @return Size of set.
+     */
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Adds element to the set.
+     *
+     * @param element Element to add.
+     */
+    @Override
+    public void add(T element) {
+        if (!alreadyExists(element)) {
+            Node<T> node = new Node<>(element);
+            if (this.head == null && this.tail == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+            this.tail = node;
+            this.size++;
+        }
+    }
+
+    /**
+     * Checks if the element exists.
+     *
+     * @param element Element to test.
+     * @return True if element already exists, false otherwise.
+     */
+    private boolean alreadyExists(T element) {
+        boolean result = false;
+        for (T setElement : this) {
+            if (setElement.equals(element)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code E}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            /**
+             * Pointer to the current node.
+             */
+            private Node<T> pointer = head;
+
+            /**
+             * hasNext implementation.
+             *
+             * @return True, if next element exists.
+             */
+            @Override
+            public boolean hasNext() {
+                return pointer != null;
+            }
+
+            /**
+             * Returns next element.
+             *
+             * @return Next element.
+             * @throws NoSuchElementException
+             */
+            @Override
+            public T next() throws NoSuchElementException {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No element left");
+                }
+                T element = pointer.element;
+                pointer = pointer.next;
+                return element;
+            }
+        };
+    }
+
+    /**
+     * Node class.
+     *
+     * @param <T> Any object.
+     * @author Vladimir Ivanov
+     * @version 0.1
+     * @since 30.08.2017
+     */
+    private static class Node<T> {
+        /**
+         * Nex node in the list.
+         */
+        @SuppressWarnings("checkstyle:visibilitymodifier")
+        Node<T> next;
+        /**
+         * Node element.
+         */
+        @SuppressWarnings("checkstyle:visibilitymodifier")
+        T element;
+
+        /**
+         * Construct node.
+         *
+         * @param element Element to store.
+         */
+        Node(T element) {
+            this.next = null;
+            this.element = element;
+        }
+    }
+}
--- chapter_005/src/test/java/ru/job4j/pro/collections/set/SimpleListSetTest.java
+++ chapter_005/src/test/java/ru/job4j/pro/collections/set/SimpleListSetTest.java
@@ -3,24 +3,26 @@ package ru.job4j.pro.collections.set;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * SimpleSet class.
+ * SimpleArraySet class.
  *
  * @author Vladimir Ivanov
  * @version 0.1
  * @since 30.08.2017
  */
-public class SimpleSetTest {
+public class SimpleListSetTest {
     /**
      * Test adding to set.
      */
     @Test
     public void whenAddElementsToSetThenGetItThere() {
-        SimpleSet<String> set = new SimpleSet<>(10);
+        SimpleSet<String> set = new SimpleListSet<>();
         set.add(new String("one"));
         set.add(new String("one"));
         set.add(new String("one"));
@@ -34,7 +36,7 @@ public class SimpleSetTest {
      */
     @Test
     public void whenAddElementToSetThenWeCanGetItViaIterator() {
-        SimpleSet<String> set = new SimpleSet<>(10);
+        SimpleSet<String> set = new SimpleListSet<>();
         set.add(new String("one"));
         set.add(new String("one"));
         set.add(new String("one"));
@@ -48,4 +50,29 @@ public class SimpleSetTest {
         assertThat(result, is("two"));
     }
 
+    /**
+     * Test that next after last element will cause NoSuchElementException.
+     */
+    @Test
+    public void whenNoElementsLeftInArraySetThenNextCauseNoSuchElementException() {
+        SimpleSet<String> set = new SimpleListSet<>();
+        set.add(new String("one"));
+        set.add(new String("one"));
+        set.add(new String("one"));
+        set.add(new String("two"));
+        set.add(new String("two"));
+
+        Iterator<String> it = set.iterator();
+        it.next();
+
+        Exception result = null;
+        try {
+            it.next();
+            it.next();
+        } catch (Exception e) {
+            result = e;
+        }
+
+        assertThat(result, instanceOf(NoSuchElementException.class));
+    }
 }
